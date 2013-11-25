5e020a89cb5a44ecb635fb3acde1dd0811e98bff
Sam Bisbee <sam@sbisbee.com>
2013-11-25 15:24:26 -0500

    Adding the design doc code.

---
+++ couchapp/app.js
@@ -0,0 +1,38 @@
+var couchapp = require('couchapp');
+var path = require('path');
+
+var ddoc = { _id:'_design/app' };
+
+ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
+  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
+    throw "Only admin can delete documents on this database.";
+  } 
+};
+
+ddoc.filters = {
+  unseen: function(doc, req) {
+    var i;
+
+    if(doc._deleted || !doc.server) {
+      return false;
+    }
+
+    if(!doc.clientsSeen || !doc.clientsSeen.length) {
+      return true;
+    }
+
+    if(!req.query || !req.query.client) {
+      return true;
+    }
+
+    for(i in doc.clientsSeen) {
+      if(doc.clientsSeen.hasOwnProperty(i) && doc.clientsSeen[i] == req.query.client) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
+
+module.exports = ddoc;
